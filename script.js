80d64f323a0fa2a837a5b3d9bc129973caa1e429
videju <77975858+videju@users.noreply.github.com>
2023-08-01 17:43:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+import { join } from 'path';
+import { createTransport } from 'nodemailer';
+import express from 'express';
+import { schedule } from 'node-cron';
+import serverless from 'serverless-http';
+
+
+// const form = document.getElementById('form');
+
+
+var myemail = 'coldplane567@gmail.com'
+var mypassword = 'wiqbilarhpyofwzm';
+const mailer = createTransport(`smtp://${myemail}:${mypassword}@smtp.gmail.com:587`)
+
+/**
+ * 
+ * @param {string} email 
+ */
+function sendMail(email) {
+    const mail = mailer.sendMail({
+        from: myemail,
+        to: email,
+        subject: "Testing Koding 101 Email",
+        html: `<html>
+        <head>
+            <title>Codebeautify.org Text to HTML Converter</title>
+        </head>
+        <body>
+            <p>dear lic users 
+    It is hereby informed that your policy will expire soon 
+    Renew It Now !!! 
+                                            -rode(lic)</p>
+        </body>
+    </html>`,
+    }
+    )
+
+    return mail
+}
+
+const app = express();
+const month = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+app.post("/schedule", (req, res) => {
+    const query = req.query;
+    const email = query.email;
+    const date = new Date(query.date);
+    console.log("task scheduled")
+    const job = schedule(` 6 16 ${date.getDate()} ${month[ date.getMonth() ]} *`, () => {
+        console.log("job started");
+        const mail = sendMail(email)
+        mail.then(() => { console.log("mail sent"); }).catch((reason) => console.log(reason))
+    })
+
+    res.json({ "message": "task scheduled" })
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname,"/index.html"))
+})
+
+
+export const handler = serverless(app)
