39520eb27a274c5d4c765d8f1228c1e6486d3faa
Riddhiman007 <91105040+Ridhiman007@users.noreply.github.com>
2023-08-05 11:08:23 +0530

    d

--- app.mjs
+++ app.mjs
@@ -58,7 +58,7 @@ app.post("/api/schedule", (req, res) => {
   const date = new Date(query.date);
   console.log("task scheduled");
   const job = schedule(
-    `* 11 ${date.getDate()} ${month[date.getMonth()]} *`,
+    `15 11 ${date.getDate()} ${month[date.getMonth()]} *`,
     () => {
       console.log("job started");
       const mail = sendMail(email);
